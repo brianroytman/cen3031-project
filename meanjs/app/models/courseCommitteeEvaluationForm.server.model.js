2700f8305049946c7f0d3432b2e35ee3eef60840
Kyle Kyrazis <kkyrazis@ufl.edu>
2014-10-19 15:28:45 -0400

    New model for the Course Committee Evaluation Form and corresponding unit tests.

--- meanjs/app/models/courseCommitteeEvaluationForm.server.model.js
+++ meanjs/app/models/courseCommitteeEvaluationForm.server.model.js
@@ -10,9 +10,174 @@ var mongoose = require('mongoose'),
 * CourseCommitteeEvaluationForm Schema
 */
 var CourseCommitteeEvaluationFormSchema = new Schema({
+	/* Found in outcomeAssessmentFormSchema
+	courseTitle: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	courseNumber: {
+		type: Number,
+		required: true
+	},
+	term: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	/*
+		This might need to be its own small schema and then we would nest it.
+	*/ /* Found in outcome assessment form schema
+	instructor: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	/*
+		For now I'll just have this as a string, but eventually might need 
+		to be a list of users or instructors or something.
+	*/
+	courseCommitteeParticipants: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	date: {
+		type: Date,
+		trim: true,
+		default: Date.now,
+		required: true
+	},
+	outcome: {
+		type: Number,
+		default: 0,
+		required: true
+	},
+	description: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	/*
+		Start of the yes or no questions
+	*/
+	syllabusReflectCurrentContent: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	droppedTopics: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	addedTopics: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	//Next set of questions
+	textbookWorkingWell: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	changesRequiredForNextAcademicYear: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	newBooksToBeEvaluated: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	bookMapWellToSyllabus: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	//next set of questions
+	otherEvaluationsIndicateIssues: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	//next set
+	didStudentsMasterMaterial: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	problemsWithKnowledgeInKeyConcepts: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	//Section II
+	prereqsStillAppropriate: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	satisfyNeedsOfFollowupCourses: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	/*
+		End of questions
+	*/
+	/*
+		Start of textBoxes
+	*/
+	//Section I
+	sectionIActionsRecommendations: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	//Section II
+	sectionIIActionsRecommendations: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	//Section III
+	recommendationsForCourseImprovement: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	recommendationsToCENProgramGovernance: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	sectionIIIRecommendationsComments: {
+		type: String,
+		trim: true,
+		default: '',
+		required: true
+	},
+	//Not sure how nesting objects works with handlebars but I think
+	//it could work like courseOutcomeAssessmentForm.courseTitle
+	//might refactor courseOutcomeAssessmentForm based off of front ends
+	//course schema. We'll see.
+	courseOutcomeAssessmentForm: { 
+		type: Schema.Types.ObjectId,
+		ref: 'CourseOutcomeAssessmentForm'
+	}
 	
-	// FILL IN SCHEMA HERE!
-
 });
 
 mongoose.model('CourseCommitteeEvaluationForm', CourseCommitteeEvaluationFormSchema);
